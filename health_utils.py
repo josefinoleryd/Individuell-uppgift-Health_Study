a5de4d5d3c44c62e4677ea3a4640fdd30c1ddfff
Josefin <josefin.oleryd@gmail.com>
2025-11-30 10:53:37 +0100

    Glömde att commita/pusha, så måååånga ändringar lol

--- health_utils.py
+++ health_utils.py
@@ -40,12 +40,44 @@ class HealthAnalyzer:
         ax.grid(axis="y", alpha=0.5)
         return fig
 
-    def plot_bar(self, column, title, xlabel, colors=None):
-        """Ritar stapeldiagram över vald kolum"""
-        counts = self.df[column].value_counts()
+    def plot_bar(self, column, title, xlabel, colors=None, mapping=None, as_percentage=False):
+        """
+        Ritar stapeldiagram över vald kolum.
+        Kan hantera namnbyte och procent.
+        """
+        if as_percentage:
+            counts = self.df[column].value_counts(normalize=True).round(4) * 100
+            ylabel = "Andel (%)"
+        else:
+            counts = self.df[column].value_counts()
+            ylabel = "Antal deltagare"
+
+        if mapping:
+            counts = counts.rename(index=mapping)
+
         fig, ax = plt.subplots(figsize=(9, 7))
         ax.bar(counts.index, counts.values, color=colors)
         ax.set_title(title)
         ax.set_xlabel(xlabel)
-        ax.set_ylabel("Antal deltagare")
+        ax.set_ylabel(ylabel)
         return fig 
+    
+    def calc_confidence_interval(self, column, n_boot=5000, confidence=0.95):
+        """
+        Beräknar konfidensintervall med Bootstrap-metoden för en given kolumn    
+        """
+        data = self.df[column]
+        n = len(data)
+        boot_means = np.empty(n_boot)
+
+        for b in range(n_boot):
+            boot_sample = np.random.choice(data, size=n, replace=True)
+            boot_means[b] = np.mean(boot_sample)
+
+        lower_p = ((1.0 - confidence) / 2.0) * 100
+        upper_p = (confidence + ((1.0 - confidence) / 2.0)) * 100
+
+        ci_min = np.percentile(boot_means, lower_p)
+        ci_max = np.percentile(boot_means, upper_p)
+
+        return ci_min, ci_max
